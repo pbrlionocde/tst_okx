4ae0d713042c330f81f7615b473dec13f4a80415
Dimcheck <dfarinyak@gmail.com>
2022-11-13 20:01:40 +0200

    added parse response

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from api_wrappers.okx import OkxClient
+from pprint import pprint
+
 
 if __name__ == '__main__':
     # init instance of OKX client
     client = OkxClient()
 
     resp = client.get_account_balance(demo=True)
-    resp
+    t = client.parse_response(resp)
+    pprint(t)
