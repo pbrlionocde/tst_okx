8db634d9818e5049165d4b969c4915c6dd8179a9
Dimcheck <dfarinyak@gmail.com>
2022-11-30 15:43:55 +0200

    added creation of log errors folder

--- src/logger/logger_conf.py
+++ src/logger/logger_conf.py
@@ -7,8 +7,10 @@ LOGGER_PATH: Final = 'src/logger/logging_errors/error.log'
 logger = logging.getLogger('error_logger')
 
 if not os.path.exists(LOGGER_PATH):
-    with open(LOGGER_PATH, 'w'):
-        print('initialized logger file!!!')
+    os.mkdir('src/logger/logging_errors')
+
+with open(LOGGER_PATH, 'w'):
+    print('initialized logger file!!!')
 
 file_handler = logging.FileHandler(LOGGER_PATH)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
